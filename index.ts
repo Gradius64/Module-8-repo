161e3f202b8567f0c726cb0ea36550af930320aa
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-17 15:57:25 -0600

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,66 @@
+// import classes
+
+
+// create an array of vehicles
+interface Vehicle {
+  type: string;
+  make: string;
+  model: string;
+  year: number;
+}
+
+// Example of accessing the array
+console.log(vehicles);
+
+// TODO: uncomment once trucks are implemented
+ // Properties of the Truck class
+
+ 
+  // Properties of the Truck class
+ 
+  // Constructor to initialize the Truck properties
+  
+
+
+ 
+
+
+  
+
+  // Truck properties and methods
+  // Properties of the Truck class
+
+  
+
+
+
+  // Truck properties and methods
+
+
+
+  
+
+  // Implementation details
+
+
+  // Truck properties and methods
+
+// const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+
+
+
+// TODO: uncomment once motorbikes are implemented
+// const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
+// const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+
+// push vehicles to array
+// TODO: uncomment once trucks are implemented
+
+// TODO: uncomment once motorbikes are implemented
+// vehicles.push(motorbike1);
+
+// create a new instance of the Cli class
+
+
+// start the cli
+
